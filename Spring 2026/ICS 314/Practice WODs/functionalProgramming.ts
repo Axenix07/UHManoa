05a0e55f6e0108a783d76edf132863142e2489fd
Austin Gardner <gardaw1@gmail.com>
2026-02-04 23:26:03 -1000

    hw

--- Spring 2026/ICS 314/Practice WODs/functionalProgramming.ts
+++ Spring 2026/ICS 314/Practice WODs/functionalProgramming.ts
@@ -673,23 +673,40 @@ function percentageHawaiian(data: UhData[]): number {
 const listCampuses = (data: UhData[]): string[] => (data.map((item) => item.campus))
 const totalDegreesByCampus = (data: UhData[], campus: string):number => data.filter((item) => item.campus === campus).reduce((acc, cur) => acc + cur.awards, 0)
 
-const listCampusDegrees = (data: UhData[]): Object[] => data.map(item=>({"campus": item.campus, "degrees": item.awards}))
 
-interface UHData {
+
+interface CampusTotal {
   campus: string;
   degrees: number;
 }
 
-function maxDegrees(data: UHData[]): number {
+function listCampusDegrees(data: UhData[]): CampusTotal[] {
+  const groupedByCampus = Object.groupBy(data, (item) => item.campus);
+
+  const results: CampusTotal[] = [];
+
+  for (const [campusName, entries] of Object.entries(groupedByCampus)) {
+    const totalDegrees = (entries ?? []).reduce((sum, entry) => sum + entry.awards, 0);
+
+    results.push({
+      campus: campusName,
+      degrees: totalDegrees
+    });
+  }
+
+  return results;
+}
+
+function maxDegrees(data: UhData[]): number {
   const degreesPerCampus: Record<string, number> = {};
 
   data.forEach((entry) => {
-    const { campus, degrees } = entry;
+    const { campus, awards } = entry;
     
     if (degreesPerCampus[campus]) {
-      degreesPerCampus[campus] += degrees;
+      degreesPerCampus[campus] += awards;
     } else {
-      degreesPerCampus[campus] = degrees;
+      degreesPerCampus[campus] = awards;
     }
   });
 
@@ -701,3 +718,5 @@ console.log(totalDegrees(uhdata));
 console.log(percentageHawaiian(uhdata));
 console.log(listCampuses(uhdata));
 console.log(totalDegreesByCampus(uhdata, "UH Manoa"));
+console.log(listCampusDegrees(uhdata));
+console.log(maxDegrees(uhdata));
